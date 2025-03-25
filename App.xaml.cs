4f33e6c410b0a625effbaaa55ff3ee98ff140d64
VargaBalint21 <varga.balint@blathy.info>
2025-03-25 11:42:06 +0100

    alapok

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using OfficeWebshopAdminPanelApp.Services;
+using OfficeWebshopAdminPanelApp.ViewModels;
+using System.Configuration;
 using System.Data;
+using System.Net.Http;
 using System.Windows;
 
 namespace OfficeWebshopAdminPanelApp
@@ -9,6 +13,25 @@ namespace OfficeWebshopAdminPanelApp
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider Services { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            Services = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton(new HttpClient { BaseAddress = new Uri("https://your-laravel-api.com/") });
+            services.AddSingleton<AuthService>();
+            services.AddTransient<LoginViewModel>();
+            services.AddSingleton<MainWindow>();
+        }
     }
 
 }
--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using OfficeWebshopAdminPanelApp.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OfficeWebshopAdminPanelApp
+namespace OfficeWebshopAdminPanelApp.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginView.xaml
     /// </summary>
-    /// //dkwodkwk
-    public partial class MainWindow : Window
+    public partial class LoginView : Window
     {
-        public MainWindow(/*LoginViewModel loginViewModel*/)
+        public LoginView()
         {
             InitializeComponent();
-            //DataContext = loginViewModel;
         }
     }
 }
